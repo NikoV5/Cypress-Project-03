083fe9bce84aede93c1009bae4cf3c40349da204
NikoV5 <NikoVourtsis7@gmail.com>
2024-05-28 13:58:37 -0500

    Added Updated Files

---
+++ cypress/e2e/integration/project-03.cy.js
@@ -0,0 +1,58 @@
+/// <reference types="cypress"/>
+describe("Cypress-Project-02 - Login Form", () => {
+  beforeEach(() => {
+    // 1
+    cy.visit("https://techglobal-training.com/frontend/project-2");
+
+
+  });
+  it('Test Case 1', () => {
+
+  })
+
+  it('Test Case 2', () => {
+
+
+
+  })
+
+  it('Test Case 3', () => {
+
+
+  })
+
+  it('Test Case 4', () => {
+
+  })
+
+  it('Test Case 5', () => {
+
+
+  })
+
+  it('Test Case 6', () => {
+
+
+  })
+
+  it('Test Case 7', () => {
+
+
+  })
+
+  it('Test Case 8', () => {
+
+
+  })
+
+  it('Test Case 9', () => {
+
+
+  })
+
+  it('Test Case 10', () => {
+
+
+  })
+});
+
